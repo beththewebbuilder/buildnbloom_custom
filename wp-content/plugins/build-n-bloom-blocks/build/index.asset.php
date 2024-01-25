815eebed92ec1bc34c6e4d51eae8639d62d4023a
beth <beththewebbuilder@gmail.com>
2024-01-25 10:21:33 +0000

    Block in new category

--- wp-content/plugins/build-n-bloom-blocks/build/index.asset.php
+++ wp-content/plugins/build-n-bloom-blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '9a9ee62c132d26c89a51');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'afabf28910e57378b3c3');
