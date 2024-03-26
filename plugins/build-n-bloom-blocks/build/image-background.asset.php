b3af0c8a1dd1cad0c9baa6f04088961770bcf84a
Bethany Fowler <beththewebbuilder@gmail.com>
2024-03-26 18:58:30 +0000

    Background build changes

--- plugins/build-n-bloom-blocks/build/image-background.asset.php
+++ plugins/build-n-bloom-blocks/build/image-background.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4a2ed0e3ae6a49c3fcd0');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7315d21d2e9bcc7000f0');
